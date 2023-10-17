710d0347029e2f34a28a3fa6be5ad1c431dc3f25
Elewi1 <141275842+Elewi1@users.noreply.github.com>
2023-10-17 23:23:31 +0100

    Update convert_c_percent_p.c

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -9,7 +9,7 @@ unsigned int convert_p(va_list args, buffer_t *output,
 
 /**
  * convert_c - Converts an argument to an unsigned char and
- *             stores it to a buffer contained in a struct.
+ *             stores the results into a buffer contained in a struct.
  * @args: A va_list pointing to the argument to be converted.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
